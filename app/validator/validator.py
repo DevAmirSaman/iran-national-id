e43c33790e8ac4e7ea0601badb043fff9860cd1a
DevAmirSaman <dev.amirsaman@gmail.com>
2025-08-29 01:08:46 +0330

    feat(validator): add validator subpackage

---
+++ app/validator/validator.py
@@ -0,0 +1,7 @@
+def is_national_id_valid(national_id: str) -> bool:
+    checksum = 0
+    for ch, weight in zip(national_id[:9], range(10, 1, -1)):
+        checksum += int(ch) * weight
+
+    check_digit = checksum % 11
+    return int(national_id[9]) == (check_digit if check_digit < 2 else 11 - check_digit)
